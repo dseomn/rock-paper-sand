af7f3dc94cb6de9c5b5834e06537e3af1822eb1c
David Mandelberg <david@mandelberg.org>
2023-09-04 15:51:40 -0400

    Add filter using the JustWatch API

--- rock_paper_sand/justwatch.py
+++ rock_paper_sand/justwatch.py
@@ -21,11 +21,38 @@ dependency, and 2) the library doesn't give enough control over the HTTP calls
 to enable useful things like caching and retrying specific errors.
 """
 
+import datetime
+from collections.abc import Set
 from typing import Any
+import warnings
 
+import dateutil.parser
 import requests
 
+from rock_paper_sand import config_pb2
+from rock_paper_sand import media_filter
+
 _BASE_URL = "https://apis.justwatch.com/content"
+
+
+def _convert_placeholder_datetime_to_none(
+    value: datetime.datetime, *, relative_url: str
+) -> datetime.datetime | None:
+    if value in (datetime.datetime(1, 1, 1, tzinfo=datetime.timezone.utc),):
+        return None
+    if value < datetime.datetime(1990, 1, 1, tzinfo=datetime.timezone.utc):
+        # https://en.wikipedia.org/wiki/Video_on_demand says "As ... in the
+        # 1990s ... which culminated in the arrival of VOD ..." so it seems
+        # unlikely that any date before 1990 is valid in the context of when
+        # JustWatch thinks something was available to stream online.
+        warnings.warn(
+            f"{_BASE_URL}/{relative_url} has a date field that's improbably "
+            f"old, {value!r}. If it looks like it might be a placeholder, "
+            "consider adding it to the _convert_placeholder_datetime_to_none "
+            "function.",
+            UserWarning,
+        )
+    return value
 
 
 class Api:
@@ -50,3 +77,78 @@ class Api:
         response_json = response.json()
         self._cache[relative_url] = response_json
         return response_json
+
+
+class Filter(media_filter.Filter):
+    """Filter based on JustWatch's API."""
+
+    def __init__(
+        self,
+        filter_config: config_pb2.JustWatchFilter,
+        *,
+        api: Api,
+    ):
+        self._config = filter_config
+        self._api = api
+
+    def _availability(self, content: Any, *, relative_url: str) -> Set[str]:
+        # TODO(dseomn): Detect and handle partial availability, e.g., when only
+        # some seasons or episodes are available.
+        availability = set()
+        now = datetime.datetime.now(tz=datetime.timezone.utc)
+        for offer in content.get("offers", ()):
+            provider = offer["package_short_name"]
+            monetization_type = offer["monetization_type"]
+            if (
+                self._config.providers
+                and provider not in self._config.providers
+            ) or (
+                self._config.monetization_types
+                and monetization_type not in self._config.monetization_types
+            ):
+                continue
+            available_from = _convert_placeholder_datetime_to_none(
+                dateutil.parser.isoparse(offer["available_from"]),
+                relative_url=relative_url,
+            )
+            available_to = _convert_placeholder_datetime_to_none(
+                dateutil.parser.isoparse(offer["available_to"]),
+                relative_url=relative_url,
+            )
+            if available_to is not None and now > available_to:
+                continue
+            comments = [monetization_type]
+            if available_from is not None and now < available_from:
+                comments.append(f"starting {available_from}")
+            if available_to is not None:
+                comments.append(f"until {available_to}")
+            # TODO(dseomn): Show the human-readable provider name instead of the
+            # short name.
+            availability.add(
+                f"{provider} ({', '.join(comments)})" if comments else provider
+            )
+        return availability
+
+    def filter(
+        self, media_item: config_pb2.MediaItem
+    ) -> media_filter.FilterResult:
+        """See base class."""
+        if not media_item.justwatch_id:
+            return media_filter.FilterResult(False)
+        relative_url = (
+            f"titles/{media_item.justwatch_id}/locale/{self._config.locale}"
+        )
+        content = self._api.get(relative_url)
+        extra_information = set()
+        if (
+            self._config.providers
+            or self._config.monetization_types
+            or self._config.any_availability
+        ):
+            availability = self._availability(
+                content, relative_url=relative_url
+            )
+            if not availability:
+                return media_filter.FilterResult(False)
+            extra_information.update(availability)
+        return media_filter.FilterResult(True, extra=extra_information)
--- rock_paper_sand/justwatch_test.py
+++ rock_paper_sand/justwatch_test.py
@@ -12,13 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from collections.abc import Mapping
+import datetime
+from typing import Any
 from unittest import mock
 
 from absl.testing import absltest
 from absl.testing import parameterized
+from google.protobuf import json_format
+import immutabledict
 import requests
 
+from rock_paper_sand import config_pb2
 from rock_paper_sand import justwatch
+from rock_paper_sand import media_filter
+
+_NOW = datetime.datetime.now(tz=datetime.timezone.utc)
+_TIME_IN_PAST_1 = _NOW - datetime.timedelta(days=1)
+_TIME_IN_FUTURE_1 = _NOW + datetime.timedelta(days=1)
+_TIME_IN_FUTURE_2 = _NOW + datetime.timedelta(days=2)
+
+
+def _offer(
+    *,
+    package_short_name: str = "some_package",
+    monetization_type: str = "some_monetization_type",
+    available_from: str = "0001-01-01T00:00:00Z",
+    available_to: str = "0001-01-01T00:00:00Z",
+) -> Any:
+    return {
+        "package_short_name": package_short_name,
+        "monetization_type": monetization_type,
+        "available_from": available_from,
+        "available_to": available_to,
+    }
 
 
 class JustWatchApiTest(parameterized.TestCase):
@@ -55,6 +82,152 @@ class JustWatchApiTest(parameterized.TestCase):
 
         self.assertEqual("foo", data)
         self.assertEmpty(self._mock_session.mock_calls)
+
+
+class FilterTest(parameterized.TestCase):
+    def setUp(self):
+        self._mock_api = mock.create_autospec(
+            justwatch.Api, spec_set=True, instance=True
+        )
+
+    @parameterized.named_parameters(
+        dict(
+            testcase_name="no_id",
+            filter_config={"locale": "en_US"},
+            media_item={"name": "foo"},
+            expected_result=media_filter.FilterResult(False),
+        ),
+        dict(
+            testcase_name="no_match_conditions",
+            filter_config={"locale": "en_US"},
+            media_item={"name": "foo", "justwatchId": "movie/1"},
+            api_data={"titles/movie/1/locale/en_US": {}},
+            expected_result=media_filter.FilterResult(True),
+        ),
+        dict(
+            testcase_name="any_availability_no_match",
+            filter_config={"locale": "en_US", "anyAvailability": True},
+            media_item={"name": "foo", "justwatchId": "movie/1"},
+            api_data={"titles/movie/1/locale/en_US": {}},
+            expected_result=media_filter.FilterResult(False),
+        ),
+        dict(
+            testcase_name="any_availability_matches",
+            filter_config={"locale": "en_US", "anyAvailability": True},
+            media_item={"name": "foo", "justwatchId": "movie/1"},
+            api_data={
+                "titles/movie/1/locale/en_US": {
+                    "offers": [
+                        _offer(
+                            package_short_name="foo",
+                            monetization_type="bar",
+                        ),
+                        _offer(
+                            package_short_name="quux",
+                            monetization_type="baz",
+                            available_from=_TIME_IN_FUTURE_1.isoformat(),
+                            available_to=_TIME_IN_FUTURE_2.isoformat(),
+                        ),
+                        _offer(available_to=_TIME_IN_PAST_1.isoformat()),
+                    ],
+                },
+            },
+            expected_result=media_filter.FilterResult(
+                True,
+                extra={
+                    "foo (bar)",
+                    (
+                        f"quux (baz, starting {_TIME_IN_FUTURE_1}, until "
+                        f"{_TIME_IN_FUTURE_2})"
+                    ),
+                },
+            ),
+        ),
+        dict(
+            testcase_name="specific_availability_matches",
+            filter_config={
+                "locale": "en_US",
+                "providers": ["foo"],
+                "monetizationTypes": ["bar"],
+            },
+            media_item={"name": "foo", "justwatchId": "movie/1"},
+            api_data={
+                "titles/movie/1/locale/en_US": {
+                    "offers": [
+                        _offer(
+                            package_short_name="foo",
+                            monetization_type="bar",
+                        ),
+                        _offer(
+                            package_short_name="hidden",
+                            monetization_type="bar",
+                        ),
+                        _offer(
+                            package_short_name="foo",
+                            monetization_type="hidden",
+                        ),
+                        _offer(
+                            package_short_name="hidden",
+                            monetization_type="hidden",
+                        ),
+                    ],
+                },
+            },
+            expected_result=media_filter.FilterResult(
+                True, extra={"foo (bar)"}
+            ),
+        ),
+    )
+    def test_filter(
+        self,
+        *,
+        filter_config: ...,
+        media_item: ...,
+        api_data: Mapping[str, Any] = immutabledict.immutabledict(),
+        expected_result: media_filter.FilterResult,
+    ):
+        self._mock_api.get.side_effect = lambda relative_url: api_data[
+            relative_url
+        ]
+        test_filter = justwatch.Filter(
+            json_format.ParseDict(filter_config, config_pb2.JustWatchFilter()),
+            api=self._mock_api,
+        )
+
+        result = test_filter.filter(
+            json_format.ParseDict(media_item, config_pb2.MediaItem())
+        )
+
+        self.assertEqual(expected_result, result)
+
+    def test_possible_unknown_placeholder_datetime(self):
+        self._mock_api.get.return_value = {
+            "offers": [
+                _offer(
+                    package_short_name="foo",
+                    monetization_type="bar",
+                    available_from="1970-01-01T00:00:00Z",
+                )
+            ]
+        }
+        test_filter = justwatch.Filter(
+            json_format.ParseDict(
+                {"locale": "en_US", "anyAvailability": True},
+                config_pb2.JustWatchFilter(),
+            ),
+            api=self._mock_api,
+        )
+
+        with self.assertWarnsRegex(UserWarning, "1970.*might be a placeholder"):
+            result = test_filter.filter(
+                json_format.ParseDict(
+                    {"name": "foo", "justwatchId": "movie/1"},
+                    config_pb2.MediaItem(),
+                )
+            )
+        self.assertEqual(
+            media_filter.FilterResult(True, extra={"foo (bar)"}), result
+        )
 
 
 if __name__ == "__main__":
