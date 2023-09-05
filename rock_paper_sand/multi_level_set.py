0a21abead52f00be2d9855cf1266a4ea06e6f394
David Mandelberg <david@mandelberg.org>
2023-09-05 17:31:07 -0400

    Add a data structure to track watched/read/etc. media

---
+++ rock_paper_sand/multi_level_set.py
@@ -0,0 +1,101 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Multi-level sets."""
+
+from collections.abc import Collection
+from typing import NewType, TypeVar
+
+_T = TypeVar("_T")
+
+# Something like a season/episode number. E.g., season 2 would be (2,) and
+# episode 3 of season 4 would be (4, 3). Earlier levels include all children, so
+# (2,) includes (2, x) for any value of x, and () includes any MultiLevelNumber.
+MultiLevelNumber = NewType("MultiLevelNumber", tuple[int, ...])
+
+# An inclusive range of MultiLevelNumber.
+MultiLevelRange = NewType(
+    "MultiLevelRange", tuple[MultiLevelNumber, MultiLevelNumber]
+)
+
+
+def _parse_multi_level_number(number_str: str) -> MultiLevelNumber:
+    if number_str == "all":
+        return MultiLevelNumber(())
+    return MultiLevelNumber(tuple(map(int, number_str.split("."))))
+
+
+def _parse_multi_level_range(range_str: str) -> MultiLevelRange:
+    start_str, sep, end_str = range_str.partition("-")
+    if not sep:
+        end_str = start_str
+    if "-" in end_str:
+        raise ValueError("Multiple hyphens in a range.")
+    return MultiLevelRange(
+        (
+            _parse_multi_level_number(start_str.strip()),
+            _parse_multi_level_number(end_str.strip()),
+        )
+    )
+
+
+def _ge_start(number: MultiLevelNumber, *, start: MultiLevelNumber) -> bool:
+    if len(number) >= len(start):
+        return number[: len(start)] >= start
+    else:
+        return number > start[: len(number)]
+
+
+def _le_end(number: MultiLevelNumber, *, end: MultiLevelNumber) -> bool:
+    if len(number) >= len(end):
+        return number[: len(end)] <= end
+    else:
+        return number < end[: len(number)]
+
+
+class MultiLevelSet:
+    """A set of MultiLevelNumber."""
+
+    def __init__(self, ranges: Collection[MultiLevelRange]):
+        self._ranges = ranges
+
+    @classmethod
+    def from_string(cls: type[_T], set_str: str, /) -> _T:
+        """Returns a set parsed from a string.
+
+        Args:
+            set_str: A comma-separated list of ranges. Each range can be of the
+                form `a-b` where `a` and `b` are dotted numbers, `a` where `a`
+                is a dotted number, or the literal string `all`. E.g.,
+                "1-1.2,1.4-5" could represent seasons 1-5 except season 1
+                episode 3.
+        """
+        if not set_str:
+            return cls(())
+        try:
+            return cls(
+                tuple(
+                    _parse_multi_level_range(range_str)
+                    for range_str in set_str.split(",")
+                )
+            )
+        except ValueError as parse_error:
+            raise ValueError(
+                f"Invalid multi level set: {set_str!r}"
+            ) from parse_error
+
+    def __contains__(self, number: MultiLevelNumber) -> bool:
+        for start, end in self._ranges:
+            if _ge_start(number, start=start) and _le_end(number, end=end):
+                return True
+        return False
---
+++ rock_paper_sand/multi_level_set_test.py
@@ -0,0 +1,202 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import absltest
+from absl.testing import parameterized
+
+from rock_paper_sand import multi_level_set
+
+
+class MultiLevelSetTest(parameterized.TestCase):
+    @parameterized.named_parameters(
+        dict(
+            testcase_name="trivial_member",
+            set_str="1.2.3",
+            number=(1, 2, 3),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="trivial_not_member",
+            set_str="1.2.3",
+            number=(4,),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="parent_not_member",
+            set_str="1.2.3",
+            number=(1, 2),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="child_member",
+            set_str="1.2.3",
+            number=(1, 2, 3, 4),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="all",
+            set_str="all",
+            number=(1, 2, 3),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="all_contains_all",
+            set_str="all",
+            number=(),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="all_with_extra",
+            set_str="1.2.3, all",
+            number=(4, 5, 6),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="empty",
+            set_str="",
+            number=(1, 2, 3),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="before_range_shorter_close",
+            set_str="1.2 - 2",
+            number=(1,),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="before_range_shorter",
+            set_str="2.2 - 3",
+            number=(1,),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="before_range_same_length",
+            set_str="2 - 3",
+            number=(1,),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="before_range_longer",
+            set_str="2 - 3",
+            number=(1, 2),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="in_range_first",
+            set_str="1 - 2",
+            number=(1,),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="in_range_child_of_first",
+            set_str="1 - 2",
+            number=(1, 1),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="in_range_middle",
+            set_str="1 - 3",
+            number=(2,),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="in_range_child_of_middle",
+            set_str="1 - 3",
+            number=(2, 1),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="in_range_parent_of_middle",
+            set_str="1.2 - 3.1",
+            number=(2,),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="in_range_last",
+            set_str="1 - 2",
+            number=(2,),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="in_range_child_of_last",
+            set_str="1 - 2",
+            number=(2, 3),
+            is_member=True,
+        ),
+        dict(
+            testcase_name="after_range_shorter_close",
+            set_str="1 - 2.2",
+            number=(2,),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="after_range_shorter",
+            set_str="1 - 2.2",
+            number=(3,),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="after_range_same_length",
+            set_str="1 - 2",
+            number=(3,),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="after_range_longer",
+            set_str="1 - 2",
+            number=(3, 4),
+            is_member=False,
+        ),
+        dict(
+            testcase_name="gap",
+            set_str="1 - 1.2, 1.4 - 1",
+            number=(1, 3),
+            is_member=False,
+        ),
+    )
+    def test_valid(
+        self,
+        *,
+        set_str: str,
+        number: tuple[int, ...],
+        is_member: bool,
+    ):
+        test_set = multi_level_set.MultiLevelSet.from_string(set_str)
+        self.assertEqual(is_member, number in test_set)
+
+    @parameterized.parameters(
+        ".",
+        "-",
+        ",",
+        "foo",
+        "foo.bar",
+        "foo-bar",
+        "foo,bar",
+        "1..2",
+        ".1",
+        "1.",
+        "1--2",
+        "-1",
+        "1-",
+        "1,,2",
+        ",1",
+        "1,",
+    )
+    def test_invalid(self, set_str: str):
+        with self.assertRaisesRegex(ValueError, "Invalid multi level set"):
+            multi_level_set.MultiLevelSet.from_string(set_str)
+
+
+if __name__ == "__main__":
+    absltest.main()
